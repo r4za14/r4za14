c6a3a0dbb1e45d6f65514da9fec16af3569289a3
r4za14 <144298263+r4za14@users.noreply.github.com>
2023-09-11 12:39:23 +0700

    Add files via upload

---
+++ boostrap.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+
+ <style>
+  .bordered {
+  border: solid;
+}
+
+ .contoh { 
+  height: 50px;
+ background-color: skyblue;
+}
+</style>
+
+  <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success sticky-top">
+ <div class="container-fluid">
+   
+    <a class="navbar-brand" href="#">
+<img src="http://3.bp.blogspot.com/-DzF7JCf9V3E/UprIcX62aDI/AAAAAAAAACY/Xhhh2kG5Acc/s1600/logo+rpl.jpg" width="50"> 
+XI PPLG 4
+</a> <buton class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+        <li class="nav-item mx-3">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+
+</nav>
+<div> 
+
+
+<body>
+<style>
+
+.banner{
+height:80vh;
+background:url('img/5.jpg');
+background-size:cover;
+background-position:center;
+}
+
+ .benner-contain{
+height:100%
+display:flex;
+align-items:center;
+}
+
+.background
+background-color:skyblue;
+width:100%;
+</style>
+
+<body>=
+<!--bennar-->
+<div class="container-fluid banner">
+
+<div class="container banner-container col-lg-6">
+
+<div class="teks-center">
+<p class="fs-1text-white">SELAMAT DATANG DI GREA BAKERY</p>
+setiap gigitan adalah sebuah kenangan manis yang takkan terlupakan.Dapatkah sekarang hanya di tokok kami.
+</p>
+</div>
+</div>
+</div>
+
+    <h1>Helow zidni aditya fahreza </h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
